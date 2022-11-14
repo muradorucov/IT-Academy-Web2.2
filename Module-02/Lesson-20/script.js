2e73e2d04b088293d34dcff9adbb1f6c372bf0dc
muradorucov <orucowmurad@gmail.com>
2022-11-14 09:23:00 +0400

    Class practice

--- Module-02/Lesson-20/script.js
+++ Module-02/Lesson-20/script.js
@@ -15,3 +15,50 @@
 // let ramin = new User("Ramin", "Mecidli", 27, "M")
 
 // console.log(ramin.getBirthday());
+
+
+
+// class Player {
+//     constructor(name, surename, age) {
+//         this.name = name,
+//             this.surename = surename,
+//             this.age = age,
+//             this.gender = "M",
+//             this.status = "offline"
+//     }
+//     isStatus(status) {
+//         this.status = status
+//     }
+
+// }
+
+
+// const user1 = new Player("Amil", "Alibayli", 24)
+// const user2 = new Player("Ramin", "Mecidli,", 27)
+
+
+// console.log(user1);
+// user2.isStatus("online");
+// console.log(user2);
+// console.log(user3);
+// console.log(user4);
+
+class Person {
+    constructor() {
+        this.name = "Cavad",
+            this.age = 40
+    }
+    getAgeChange() {
+       this.age += 1
+    }
+
+}
+
+
+const person1 = new Person()
+console.log(person1)
+person1.getAgeChange()
+
+console.log(person1.getAgeChange())
+
+// console.log();
